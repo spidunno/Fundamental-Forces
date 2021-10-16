3f03f87c910dce535b3fd31cf7dc3590206a097a
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2021-10-16 18:43:14 +0200

    Fallstar command properly summons starfalls :)

--- src/main/java/com/project_esoterica/empirical_esoterica/common/command/FallStarCommand.java
+++ src/main/java/com/project_esoterica/empirical_esoterica/common/command/FallStarCommand.java
@@ -1,19 +1,21 @@
 package com.project_esoterica.empirical_esoterica.common.command;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.project_esoterica.empirical_esoterica.common.worldevent.WorldEventManager;
+import com.project_esoterica.empirical_esoterica.common.worldevent.starfall.StarfallInstance;
 import com.project_esoterica.empirical_esoterica.common.worldevent.starfall.StarfallResult;
-import com.project_esoterica.empirical_esoterica.core.data.SpaceModLang;
 import com.project_esoterica.empirical_esoterica.core.registry.worldevent.StarfallResults;
 import com.project_esoterica.empirical_esoterica.core.systems.command.StarfallResultArgumentType;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
-import net.minecraft.commands.arguments.coordinates.Vec3Argument;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.level.GameRules;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 
 public class FallStarCommand {
     public FallStarCommand() {
@@ -24,12 +26,22 @@ public class FallStarCommand {
                 .requires(cs -> cs.hasPermission(2))
                 .then(Commands.argument("countdown", IntegerArgumentType.integer(0))
                         .then(Commands.argument("result", new StarfallResultArgumentType())
-                                .then(Commands.argument("position", Vec3Argument.vec3())
+                                .then(Commands.argument("position", BlockPosArgument.blockPos())
                                         .executes(context -> {
+                                            CommandSourceStack source = context.getSource();
+                                            StarfallResult result = StarfallResults.STARFALL_RESULTS.get(context.getArgument("result", String.class));
+                                            ServerLevel level = source.getLevel();
+                                            BlockPos pos = BlockPosArgument.getSpawnablePos(context,"position");
+                                            WorldEventManager.addWorldEvent(level, new StarfallInstance(result, level, pos), false);
                                             return 1;
                                         }))
                                 .then(Commands.argument("target", EntityArgument.player())
                                         .executes(context -> {
+                                            CommandSourceStack source = context.getSource();
+                                            StarfallResult result = StarfallResults.STARFALL_RESULTS.get(context.getArgument("result", String.class));
+                                            ServerLevel level = source.getLevel();
+                                            Player target = EntityArgument.getPlayer(context, "target");
+                                            WorldEventManager.addWorldEvent(level, new StarfallInstance(result, level, target), false);
                                             return 1;
                                         }))));
     }
