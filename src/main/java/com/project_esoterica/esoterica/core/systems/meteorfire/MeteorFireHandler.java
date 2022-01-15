82221771286ca0eb7f7bad986116190c567eef1a
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-01-15 16:29:47 +0100

    Very quick fix

--- src/main/java/com/project_esoterica/esoterica/core/systems/meteorfire/MeteorFireHandler.java
+++ src/main/java/com/project_esoterica/esoterica/core/systems/meteorfire/MeteorFireHandler.java
@@ -23,7 +23,7 @@ import net.minecraft.world.entity.player.Player;
 public class MeteorFireHandler {
 
     public static void entityUpdate(Entity entity) {
-        if (entity instanceof Player) {
+        if (entity != null) {
             EntityDataCapability.getCapability(entity).ifPresent(c -> {
                 if (c.meteorFireInstance != null && c.meteorFireInstance.isValid()) {
                     c.meteorFireInstance.tick(entity);
