e611ddcd84e2222fe3879b95932e50554ac5bebb
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-03-11 21:15:02 +0100

    Quick Change

--- src/main/java/com/sammy/fundamental_forces/client/renderers/entity/wraith/StoneWraithRenderer.java
+++ src/main/java/com/sammy/fundamental_forces/client/renderers/entity/wraith/StoneWraithRenderer.java
@@ -8,25 +8,12 @@ import gg.moonflower.pollen.pinwheel.api.client.animation.AnimatedEntityRenderer
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-/**
- * @author Ocelot
- */
 public class StoneWraithRenderer extends AnimatedEntityRenderer<StoneWraith> {
     private static final ResourceLocation[] DEFAULT_ANIMATIONS = new ResourceLocation[]{DataHelper.prefix("wraith.idle")};
     private static final ResourceLocation WRAITH_LOCATION = DataHelper.prefix("stone_wraith");
 
     public StoneWraithRenderer(EntityRendererProvider.Context context) {
         super(context, DataHelper.prefix("stone_wraith"), 0.5F);
-    }
-
-    @Override
-    protected void setupRotations(StoneWraith entity, PoseStack matrixStack, float ticksExisted, float rotY, float partialTicks) {
-        super.setupRotations(entity, matrixStack, ticksExisted, rotY, partialTicks);
-//        if (!((double) entity.animationSpeed < 0.01)) {
-//            float j = entity.animationPosition - entity.animationSpeed * (1.0F - partialTicks) + 6.0F;
-//            float k = (Math.abs(j % 13.0F - 6.5F) - 3.25F) / 3.25F;
-//            matrixStack.mulPose(Vector3f.ZP.rotationDegrees(6.5F * k));
-//        }
     }
 
     @Override
@@ -37,7 +24,7 @@ public class StoneWraithRenderer extends AnimatedEntityRenderer<StoneWraith> {
     }
 
     @Override
-    public ResourceLocation getTextureTableLocation(StoneWraith yeti) {
+    public ResourceLocation getTextureTableLocation(StoneWraith entity) {
         return WRAITH_LOCATION;
     }
 }
