eb791a1ecc45a76fe5e7ae48e6c35b8db79949a7
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-05-12 17:56:22 +0200

    Cleanup

--- src/main/java/com/sammy/fufo/client/renderers/block/AnchorRenderer.java
+++ src/main/java/com/sammy/fufo/client/renderers/block/AnchorRenderer.java
@@ -17,15 +17,16 @@ import static com.sammy.ortus.handlers.RenderHandler.DELAYED_RENDER;
 
 public class AnchorRenderer implements BlockEntityRenderer<AnchorBlockEntity> {
 
-    private static final ResourceLocation TEST_BEAM  = prefix("textures/vfx/uv_test.png");
+    private static final ResourceLocation TEST_BEAM = prefix("textures/vfx/uv_test.png");
     private static final RenderType TEST_BEAM_TYPE = OrtusRenderTypeRegistry.ADDITIVE_TEXTURE.apply(TEST_BEAM);
 
     public AnchorRenderer(BlockEntityRendererProvider.Context context) {
     }
+
     @Override
     public void render(AnchorBlockEntity blockEntityIn, float partialTicks, PoseStack poseStack, MultiBufferSource bufferIn, int combinedLightIn, int combinedOverlayIn) {
         VertexConsumer beamConsumer = DELAYED_RENDER.getBuffer(TEST_BEAM_TYPE);
-        blockEntityIn.NEARBY_ANCHORS.forEach(anchor -> {
+        blockEntityIn.nearbyAnchors.forEach(anchor -> {
             poseStack.pushPose();
             RenderHelper.create().renderBeam(beamConsumer, poseStack, DataHelper.fromBlockPos(blockEntityIn.getBlockPos()), DataHelper.fromBlockPos(anchor.getBlockPos()), 0.1f).setOffset(0.5f, 0.5f, 0.5f);
             poseStack.popPose();
--- src/main/java/com/sammy/fufo/common/blockentity/AnchorBlockEntity.java
+++ src/main/java/com/sammy/fufo/common/blockentity/AnchorBlockEntity.java
@@ -2,24 +2,24 @@ package com.sammy.fufo.common.blockentity;
 
 import com.sammy.fufo.core.setup.content.block.BlockEntityRegistry;
 import com.sammy.ortus.helpers.BlockHelper;
-import com.sammy.ortus.helpers.RenderHelper;
 import com.sammy.ortus.systems.blockentity.OrtusBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AnchorBlockEntity extends OrtusBlockEntity {
 
-    public static List<AnchorBlockEntity> NEARBY_ANCHORS = new ArrayList<AnchorBlockEntity>(2);
+    public ArrayList<AnchorBlockEntity> nearbyAnchors = new ArrayList<>();
+    public ArrayList<BlockPos> nearbyAnchorPositions = new ArrayList<>();
 
     public AnchorBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
@@ -30,11 +30,43 @@ public class AnchorBlockEntity extends OrtusBlockEntity {
     }
 
     @Override
+    protected void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+
+        if (!nearbyAnchorPositions.isEmpty()) {
+            CompoundTag compound = new CompoundTag();
+            compound.putInt("anchorAmount", nearbyAnchorPositions.size());
+            for (int i = 0; i < nearbyAnchorPositions.size(); i++) {
+                BlockHelper.saveBlockPos(compound, nearbyAnchorPositions.get(i), "" + i);
+            }
+            pTag.put("anchorData", compound);
+        }
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+        nearbyAnchorPositions.clear();
+        nearbyAnchors.clear();
+        CompoundTag compound = pTag.getCompound("anchorData");
+        int amount = compound.getInt("anchorAmount");
+        for (int i = 0; i < amount; i++) {
+            BlockPos pos = BlockHelper.loadBlockPos(compound, "" + i);
+            if (level != null && level.getBlockEntity(pos) instanceof AnchorBlockEntity anchor) {
+                nearbyAnchorPositions.add(pos);
+                nearbyAnchors.add(anchor);
+            }
+        }
+    }
+
+    @Override
     public void onPlace(LivingEntity placer, ItemStack stack) {
-        if(!level.isClientSide) {
-            NEARBY_ANCHORS = BlockHelper.getBlockEntities(AnchorBlockEntity.class, level, this.getBlockPos(), 4);
-            NEARBY_ANCHORS.removeIf(anchor -> anchor == this);
-            placer.sendMessage(Component.nullToEmpty("Found " + NEARBY_ANCHORS.size() + " anchors"), placer.getUUID());
+        if (!level.isClientSide) {
+            nearbyAnchors = BlockHelper.getBlockEntities(AnchorBlockEntity.class, level, this.getBlockPos(), 4);
+            nearbyAnchors.remove(this);
+            nearbyAnchorPositions = nearbyAnchors.stream().map(BlockEntity::getBlockPos).collect(Collectors.toCollection(ArrayList::new));
+            BlockHelper.updateState(level, getBlockPos());
+            placer.sendMessage(Component.nullToEmpty("Found " + nearbyAnchors.size() + " anchors"), placer.getUUID());
         }
     }
 }
