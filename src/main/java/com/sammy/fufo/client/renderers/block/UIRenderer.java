978223a1cf647aa16b43a47423a94dd47e3b6041
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-05-20 22:39:19 +0200

    Fix errors

--- src/main/java/com/sammy/fufo/client/renderers/block/UIRenderer.java
+++ src/main/java/com/sammy/fufo/client/renderers/block/UIRenderer.java
@@ -3,5 +3,5 @@ package com.sammy.fufo.client.renderers.block;
 import com.sammy.fufo.common.blockentity.UITestBlockEntity;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 
-public class UIRenderer implements BlockEntityRenderer<UITestBlockEntity> {
+public abstract class UIRenderer implements BlockEntityRenderer<UITestBlockEntity> {
 }
--- src/main/java/com/sammy/fufo/core/setup/content/block/BlockEntityRegistry.java
+++ src/main/java/com/sammy/fufo/core/setup/content/block/BlockEntityRegistry.java
@@ -3,7 +3,6 @@ package com.sammy.fufo.core.setup.content.block;
 import com.sammy.fufo.FufoMod;
 import com.sammy.fufo.client.renderers.block.AnchorRenderer;
 import com.sammy.fufo.client.renderers.block.OrbRenderer;
-import com.sammy.fufo.client.renderers.block.UIRenderer;
 import com.sammy.fufo.common.block.*;
 import com.sammy.fufo.common.blockentity.*;
 import net.minecraft.world.level.block.Block;
@@ -24,7 +23,7 @@ public class BlockEntityRegistry {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, FufoMod.FUFO);
     public static final RegistryObject<BlockEntityType<MeteorFlameBlockEntity>> METEOR_FLAME = BLOCK_ENTITY_TYPES.register("meteor_flame", () -> BlockEntityType.Builder.of(MeteorFlameBlockEntity::new, getBlocks(MeteorFlameBlock.class)).build(null));
     public static final RegistryObject<BlockEntityType<OrbBlockEntity>> ORB = BLOCK_ENTITY_TYPES.register("orb", () -> BlockEntityType.Builder.of(OrbBlockEntity::new, getBlocks(OrbBlock.class)).build(null));
-    public static final RegistryObject<BlockEntityType<AnchorBlockEntity>> ANCHOR = BLOCK_ENTITY_TYPES.register("anchor", () -> BlockEntityType.Builder.of(AnchorBlockEntity::new, getBlocks(AnchorBlock.class)).build(null));
+    public static final RegistryObject<BlockEntityType<AnchorBlockEntity>> ANCHOR = BLOCK_ENTITY_TYPES.register("anchor", () -> BlockEntityType.Builder.of(AnchorBlockEntity::new, getBlocks(PipeAnchorBlock.class)).build(null));
     public static final RegistryObject<BlockEntityType<BurnerExtractorBlockEntity>> BURNER_EXTRACTOR = BLOCK_ENTITY_TYPES.register("burner_extractor", () -> BlockEntityType.Builder.of(BurnerExtractorBlockEntity::new, getBlocks(BurnerExtractorBlock.class)).build(null));
     public static final RegistryObject<BlockEntityType<UITestBlockEntity>> UI_TEST_BLOCK = BLOCK_ENTITY_TYPES.register("ui_test_block",()-> BlockEntityType.Builder.of(UITestBlockEntity::new, getBlocks(UITestBlock.class)).build(null));
 
