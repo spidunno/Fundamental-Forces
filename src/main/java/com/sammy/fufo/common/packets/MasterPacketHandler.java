ac2af4ca8d1512a6065715b884583ea3487ac1fe
antrobot1234 <antrobot1234@gmail.com>
2022-05-21 15:51:27 -0500

    cleaned slate for new implementation of packet managing

---
+++ src/main/java/com/sammy/fufo/common/packets/MasterPacketHandler.java
@@ -0,0 +1,13 @@
+package com.sammy.fufo.common.packets;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkEvent;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+import java.util.function.Supplier;
+
+public class MasterPacketHandler {
+
+}
