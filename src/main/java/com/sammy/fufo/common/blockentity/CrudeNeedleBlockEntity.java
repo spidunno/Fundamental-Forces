cfcd6d99baaf52ec6bd6885cb54ed86cb206564c
amo <lunarteclipse@gmail.com>
2022-05-25 16:19:53 +0100

    name change

--- src/main/java/com/sammy/fufo/common/blockentity/CrudeNeedleBlockEntity.java
+++ src/main/java/com/sammy/fufo/common/blockentity/CrudeNeedleBlockEntity.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CrudeNeedleBlockEntity extends OrtusBlockEntity {
-    private ArrayList<WeaveEntity> primedBindableEntities = new ArrayList<>();
+    private ArrayList<WeaveEntity> bindableEntities = new ArrayList<>();
     public CrudeNeedleBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
@@ -32,15 +32,15 @@ public class CrudeNeedleBlockEntity extends OrtusBlockEntity {
         if(!level.isClientSide) {
             level.getEntities(new WeaveEntity(level), AABB.ofSize(Vec3.atCenterOf(this.getBlockPos()), 2, 2, 2)).forEach(entity -> {
                 if(entity instanceof WeaveEntity) {
-                    primedBindableEntities.add((WeaveEntity) entity);
+                    bindableEntities.add((WeaveEntity) entity);
                     entity.kill();
                 }
             });
-            System.out.println(primedBindableEntities.toString());
-            if (!primedBindableEntities.isEmpty()) {
+            System.out.println(bindableEntities.toString());
+            if (!bindableEntities.isEmpty()) {
                 WeaveEntity weave = new WeaveEntity(level);
-                weave.weave = new WeaveRecipe((Bindable) Arrays.stream(primedBindableEntities.get(0).weave.getBindables().toArray()).findFirst().get(), FufoMod.fufoPath("bogus"), "weak").setOutput(Items.HORSE_SPAWN_EGG.getDefaultInstance());
-                weave.weave.add((Bindable) Arrays.stream(primedBindableEntities.get(0).weave.getBindables().toArray()).findFirst().get(), new BindingType(FufoMod.fufoPath("weak")), new Vec3i(0, 0, 1), (Bindable) Arrays.stream(primedBindableEntities.get(1).weave.getBindables().toArray()).findFirst().get());
+                weave.weave = new WeaveRecipe((Bindable) Arrays.stream(bindableEntities.get(0).weave.getBindables().toArray()).findFirst().get(), FufoMod.fufoPath("bogus"), "weak").setOutput(Items.HORSE_SPAWN_EGG.getDefaultInstance());
+                weave.weave.add((Bindable) Arrays.stream(bindableEntities.get(0).weave.getBindables().toArray()).findFirst().get(), new BindingType(FufoMod.fufoPath("weak")), new Vec3i(0, 0, 1), (Bindable) Arrays.stream(bindableEntities.get(1).weave.getBindables().toArray()).findFirst().get());
                 weave.setPos(this.getBlockPos().getX() + 0.5, this.getBlockPos().getY() + 1.5, this.getBlockPos().getZ() + 0.5);
                 level.addFreshEntity(weave);
                 return InteractionResult.SUCCESS;
