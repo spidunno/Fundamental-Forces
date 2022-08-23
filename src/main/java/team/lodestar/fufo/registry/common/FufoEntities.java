9537790bdaffc4accc4224df126092516704b8df
EpicSquid <epicsquid315@gmail.com>
2022-08-23 21:57:45 +1000

    Disabled stone wraith till pollen is ready

--- src/main/java/team/lodestar/fufo/registry/common/FufoEntities.java
+++ src/main/java/team/lodestar/fufo/registry/common/FufoEntities.java
@@ -5,7 +5,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,25 +17,24 @@ import team.lodestar.fufo.client.rendering.entity.weave.HologramWeaveEntityRende
 import team.lodestar.fufo.client.rendering.entity.weave.WeaveEntityRenderer;
 import team.lodestar.fufo.client.rendering.entity.wisp.SparkEntityRenderer;
 import team.lodestar.fufo.client.rendering.entity.wisp.WispEntityRenderer;
-import team.lodestar.fufo.client.rendering.entity.wraith.StoneWraithRenderer;
 import team.lodestar.fufo.common.entity.falling.FallingCrashpodEntity;
 import team.lodestar.fufo.common.entity.magic.spell.tier1.SpellBolt;
 import team.lodestar.fufo.common.entity.weave.HologramWeaveEntity;
 import team.lodestar.fufo.common.entity.weave.WeaveEntity;
 import team.lodestar.fufo.common.entity.wisp.SparkEntity;
 import team.lodestar.fufo.common.entity.wisp.WispEntity;
-import team.lodestar.fufo.common.entity.wraith.StoneWraith;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class FufoEntities {
-    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, FufoMod.FUFO);
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, FufoMod.FUFO);
 
     public static final RegistryObject<EntityType<FallingCrashpodEntity>> FALLING_CRASHPOD = register("falling_crashpod", EntityType.Builder.<FallingCrashpodEntity>of((t, l) -> new FallingCrashpodEntity(l), MobCategory.MISC).sized(0.5f, 0.5f));
 
     public static final RegistryObject<EntityType<SparkEntity>> METEOR_FIRE_SPARK = register("spark", EntityType.Builder.<SparkEntity>of((t, l) -> new SparkEntity(l), MobCategory.MISC).sized(0.25f, 0.25f));
     public static final RegistryObject<EntityType<WispEntity>> METEOR_FIRE_WISP = register("wisp", EntityType.Builder.<WispEntity>of((t, l) -> new WispEntity(l), MobCategory.MISC).sized(0.5f, 0.5f));
 
-    public static final RegistryObject<EntityType<StoneWraith>> STONE_WRAITH = register("stone_wraith", EntityType.Builder.<StoneWraith>of((t, l) -> new StoneWraith(l), MobCategory.MONSTER).sized(0.6F, 1.95F).clientTrackingRange(6));
+    // TODO: Pollen 1.19
+//    public static final RegistryObject<EntityType<StoneWraith>> STONE_WRAITH = register("stone_wraith", EntityType.Builder.<StoneWraith>of((t, l) -> new StoneWraith(l), MobCategory.MONSTER).sized(0.6F, 1.95F).clientTrackingRange(6));
 
     public static final RegistryObject<EntityType<HologramWeaveEntity>> HOLOGRAM_WEAVE = register("hologram_weave", EntityType.Builder.<HologramWeaveEntity>of((t, l) -> new HologramWeaveEntity(l), MobCategory.MISC).sized(1.0F, 1.0F));
     public static final RegistryObject<EntityType<WeaveEntity>> BASIC_WEAVE = register("basic_weave", EntityType.Builder.<WeaveEntity>of((t, l) -> new WeaveEntity(l), MobCategory.MISC).sized(1.0F, 1.0F));
@@ -44,11 +42,11 @@ public class FufoEntities {
     // SPELLS
     public static final RegistryObject<EntityType<SpellBolt>> SPELL_BOLT = register("spell_bolt", EntityType.Builder.<SpellBolt>of((t, l) -> new SpellBolt(l), MobCategory.MISC).sized(0.1F, 0.1F));
 
-
-    @SubscribeEvent
-    public static void assignAttributes(EntityAttributeCreationEvent event) {
-        event.put(STONE_WRAITH.get(), StoneWraith.createAttributes().build());
-    }
+// TODO: Pollen 1.19
+//    @SubscribeEvent
+//    public static void assignAttributes(EntityAttributeCreationEvent event) {
+//        event.put(STONE_WRAITH.get(), StoneWraith.createAttributes().build());
+//    }
 
     private static <T extends Entity> RegistryObject<EntityType<T>> register(String id, EntityType.Builder<T> builder) {
         return ENTITY_TYPES.register(id, () -> builder.build(id));
@@ -61,7 +59,8 @@ public class FufoEntities {
             event.registerEntityRenderer(FufoEntities.FALLING_CRASHPOD.get(), FallingStarRenderer::new);
             event.registerEntityRenderer(FufoEntities.METEOR_FIRE_SPARK.get(), SparkEntityRenderer::new);
             event.registerEntityRenderer(FufoEntities.METEOR_FIRE_WISP.get(), WispEntityRenderer::new);
-            event.registerEntityRenderer(FufoEntities.STONE_WRAITH.get(), StoneWraithRenderer::new);
+            // TODO: Pollen 1.19
+//            event.registerEntityRenderer(FufoEntities.STONE_WRAITH.get(), StoneWraithRenderer::new);
             event.registerEntityRenderer(FufoEntities.HOLOGRAM_WEAVE.get(), HologramWeaveEntityRenderer::new);
             event.registerEntityRenderer(FufoEntities.BASIC_WEAVE.get(), WeaveEntityRenderer::new);
             event.registerEntityRenderer(FufoEntities.SPELL_BOLT.get(), MissileProjectileRenderer::new);
