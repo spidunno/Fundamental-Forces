1ed368bcb630fc1f7b3177602e92c2513c284ea9
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-09-22 15:46:51 +0200

    Further Crater generation changes

--- src/main/java/team/lodestar/fufo/common/starfall/impact/features/MeteoriteFeature.java
+++ src/main/java/team/lodestar/fufo/common/starfall/impact/features/MeteoriteFeature.java
@@ -20,6 +20,7 @@ import team.lodestar.fufo.registry.common.FufoBlocks;
 import team.lodestar.lodestone.helpers.BlockHelper;
 import team.lodestar.lodestone.helpers.DataHelper;
 import team.lodestar.lodestone.helpers.VecHelper;
+import team.lodestar.lodestone.systems.worldgen.LodestoneBlockFiller;
 
 import java.util.*;
 
@@ -39,45 +40,48 @@ public class MeteoriteFeature extends Feature<NoneFeatureConfiguration> {
     private static final PerlinSimplexNoise NOISE = new PerlinSimplexNoise(new WorldgenRandom(new LegacyRandomSource(1234L)), ImmutableList.of(0));
 
     public static boolean perlinNoiseTest(WorldGenLevel level, ChunkGenerator generator, BlockPos pos, RandomSource random) {
-        //TODO: this is somewhat expensive, and also just a test. Here's what we need to work on:
-        // Noise is expensive! At least when used so much. We should precompute some noise values for various angles our theta might be, and then just use the closest one depending on what theta we are at.
-        // Y levels, our craters are 3d not 2d. If we precompute our noise then we can simply reduce it's strength as we delve into the lower levels of the crater.
-
-
-        int x = pos.getX();
-        int z = pos.getZ();
-        int radius = 32;
-        int effectiveRadius = 16;
-        float noiseExponent = 5f;
-        Set<BlockPos> set = new HashSet<>();
+        LodestoneBlockFiller filler = new LodestoneBlockFiller(false);
         Map<Integer, Double> noiseValues = new HashMap<>();
         for (int i = 0; i <= 360; i++) {
-            noiseValues.put(i, NOISE.getValue(pos.getX() + pos.getZ() + i * 0.025f, pos.getY() / 0.05f, true) * noiseExponent);
+            noiseValues.put(i, NOISE.getValue(pos.getX() + pos.getZ() + i * 0.025f, pos.getY() / 0.05f, true) * 3.5f);
         }
-        double highest = 0;
+        int radius = 32;
+        int effectiveRadius = 16;
+        int depth = 6;
+        float step = 1f / depth;
+        for (int i = -3; i < depth; i++) {
+            BlockPos layerCenter = pos.below(i);
+            float radiusMultiplier = (depth - i) * step;
+            generateCraterLayer(level, filler, layerCenter, (int)(radius*radiusMultiplier), (int) (effectiveRadius*radiusMultiplier), radiusMultiplier, noiseValues);
+        }
+        filler.fill(level);
+
+        return true;
+    }
+
+    public static void generateCraterLayer(WorldGenLevel level, LodestoneBlockFiller filler, BlockPos center, int radius, int effectiveRadius, float noiseIntensity, Map<Integer, Double> noiseValues) {
+        int x = center.getX();
+        int z = center.getZ();
+        BlockPos.MutableBlockPos blockPos = new BlockPos.MutableBlockPos();
         for (int i = 0; i < radius * 2 + 1; i++) {
             for (int j = 0; j < radius * 2 + 1; j++) {
-                level.setBlock(pos.offset(i-radius, 10, j-radius), Blocks.GREEN_WOOL.defaultBlockState(), 3);
                 int xp = x + i - radius;
                 int zp = z + j - radius;
+                blockPos.set(xp, center.getY(), zp);
+                if (level.getBlockState(blockPos).isAir()) {
+                    continue;
+                }
                 double theta = 180 + 180 / Math.PI * Math.atan2(x - xp, z - zp);
-                double naturalNoiseValue = noiseValues.get(Mth.floor(theta));
+                double naturalNoiseValue = noiseValues.get(Mth.floor(theta)) * noiseIntensity;
                 if (naturalNoiseValue > 1f) {
                     naturalNoiseValue *= naturalNoiseValue;
                 }
-                if (highest < naturalNoiseValue) {
-                    highest = naturalNoiseValue;
-                }
                 int floor = (int) Math.floor(pointDistancePlane(xp, zp, x, z));
-                if (floor <= (effectiveRadius + Math.floor(naturalNoiseValue) - 1) && floor >= (effectiveRadius + Math.floor(naturalNoiseValue) - 3)) {
-                    set.add(new BlockPos(xp, pos.getY() + 10, zp));
+                if (floor <= (effectiveRadius + Math.floor(naturalNoiseValue) - 1)) {
+                    filler.entries.add(new LodestoneBlockFiller.BlockStateEntry(Blocks.AIR.defaultBlockState(), blockPos.immutable()));
                 }
             }
         }
-        set.forEach(p -> {
-            level.setBlock(p, Blocks.RED_WOOL.defaultBlockState(), 3);
-        });
-        return true;
     }
 
     public static float pointDistancePlane(double x1, double z1, double x2, double z2) {
